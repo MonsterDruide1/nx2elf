d9292e9b8fffc8a6eef03db55393576652766448
Léo Lam <leo@leolam.fr>
2021-07-22 22:23:40 +0200

    Add support for uncompressed NSOs

--- nx2elf.cpp
+++ nx2elf.cpp
@@ -117,7 +117,7 @@ struct NsoFile {
     u8 magic[4];
     u32 field_4;
     u32 field_8;
-    u32 field_c;
+    u32 flags;
     SegmentHeader segments[kNumSegment];
     // value from .note, can be various lengths :/
     std::array<u8, 32> gnu_build_id;
@@ -174,11 +174,11 @@ struct NsoFile {
 
 #define FMT_FIELD(f) p += sprintf(p, #f ": %8x\n", header.f);
 
-    if (verbose) {
-      FMT_FIELD(field_4);
-      FMT_FIELD(field_8);
-      FMT_FIELD(field_c);
-    }
+		if (verbose) {
+			FMT_FIELD(field_4);
+			FMT_FIELD(field_8);
+			FMT_FIELD(flags);
+		}
 
     p += sprintf(p, "gnu_build_id: ");
     p = FormatBytes(p, header.gnu_build_id);
@@ -264,10 +264,14 @@ struct NsoFile {
       for (int i = 0; i < kNumSegment; i++) {
         auto& seg = header.segments[i];
         auto& file_size = header.segment_file_sizes[i];
-        if (!Decompress(&image[seg.mem_offset], seg.mem_size,
-                        &file[seg.file_offset], file_size)) {
-          return false;
-        }
+				if ((header.flags & (1 << i)) != 0) {
+					if (!Decompress(&image[seg.mem_offset], seg.mem_size,
+						&file[seg.file_offset], file_size)) {
+						return false;
+					}
+				} else {
+					std::memcpy(&image[seg.mem_offset], &file[seg.file_offset], file_size);
+				}
       }
       file_type = kNso;
     } else if (file.size() >= nro_offset + sizeof(NroHeader) &&
